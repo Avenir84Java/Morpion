23a5930b9279932d998d850484bd5c0f5b1504a1
philippekurz <philippe.kurz@gmail.com>
2021-11-23 16:37:13 +0100

    Supression de ligne vides

--- Grille.java
+++ Grille.java
@@ -4,8 +4,6 @@ import java.util.Random;
 import java.util.Scanner;
 import java.lang.Math;
 
-
-
 public class Grille {
 	private final int DIMENSION_DEFAUT = 3;
 	private int DIMENSION_CUSTOM = -1;
