edf7f2a90e9b3181ba5054a536d143180ea60ca8
philippekurz <philippe.kurz@gmail.com>
2021-11-24 12:08:27 +0100

    Joueur PK

--- Joueur.java
+++ Joueur.java
@@ -1,50 +1,45 @@
-
  class Joueur {
 	private char symbole;
 	private String nom;
 	private String prenom;
     private int score;
-
 	
-	public Joueur(char symbole,String prenom,String nom) {
-		
-		this.symbole= symbole;
-		this.nom= nom;
-		this.prenom=prenom;
+	public Joueur(char symbole, String prenom, String nom) {
+		this.symbole = symbole;
+		this.nom = nom;
+		this.prenom = prenom;
 	}
-		
-		
+				
 	public String getnom() {
-       return nom;
-   }
+		return nom;
+	}
 
-   public void setnom(String nom) {
-       this.nom = nom;
-   }
+	public void setnom(String nom) {
+		this.nom = nom;
+	}
 	
 	public String getprenom() {
-       return prenom;
-   }
+		return prenom;
+	}
 
-   public void setprenom(String prenom) {
-       this.prenom = prenom;
-   }
+	public void setprenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
 	public int getscore() {
-       return score;
-   }
+		return score;
+	}
 
-  
-   public char getsymbole() {
-       return symbole;
-   }
+	public char getsymbole() {
+		return symbole;
+	}
 
-   public void setsymbole(char symbole) {
-       this.symbole = symbole;
-   }
+	public void setsymbole(char symbole) {
+		this.symbole = symbole;
+	}
 
 	public void afficher (){
-	   System.out.println("symbole"+"    "+symbole+"     "+"prenom"+"     "+prenom+"     "+"nom"+"    "+nom);
-		
+	   System.out.println("symbole"+"    "+symbole+"     "+"prenom"+"     "+prenom+"     "+"nom"+"    "+nom);		
 	}
 	
 }
